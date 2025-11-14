2e46531103ffc62cf8fed1ce68a5f145fe454ecc
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2025-11-14 20:35:02 +0700

    setup datastore

---
+++ app/src/main/java/id/bytezilla/borutoapp/di/RepositoryModule.kt
@@ -0,0 +1,34 @@
+package id.bytezilla.borutoapp.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import id.bytezilla.borutoapp.data.pref.DataStoreOperationsImpl
+import id.bytezilla.borutoapp.domain.repository.DataStoreOperations
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideDataStoreOperations(
+        @ApplicationContext context: Context
+    ): DataStoreOperations {
+        return DataStoreOperationsImpl(context = context)
+    }
+
+//    @Provides
+//    @Singleton
+//    fun provideUseCases(repository: Repository): UseCases {
+//        return UseCases(
+//            saveOnBoardingUseCase = SaveOnBoardingUseCase(repository),
+//            readOnBoardingUseCase = ReadOnBoardingUseCase(repository)
+//        )
+//    }
+
+}
