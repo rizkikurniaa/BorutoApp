5d4b72361077f12df6c68bdacd4266903208da12
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2025-11-14 16:11:47 +0700

    done welcome screen

--- app/src/main/java/id/bytezilla/borutoapp/navigation/NavGraph.kt
+++ app/src/main/java/id/bytezilla/borutoapp/navigation/NavGraph.kt
@@ -1,25 +1,30 @@
 package id.bytezilla.borutoapp.navigation
 
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.google.accompanist.pager.ExperimentalPagerApi
 import id.bytezilla.borutoapp.presentation.screens.splash.SplashScreen
+import id.bytezilla.borutoapp.presentation.screens.welcome.WelcomeScreen
 import id.bytezilla.borutoapp.util.Constants.DETAILS_ARGUMENT_KEY
 
+@ExperimentalAnimationApi
+@ExperimentalPagerApi
 @Composable
 fun SetupNavGraph(navController: NavHostController){
     NavHost(
         navController = navController,
-        startDestination = Screen.Splash.route
+        startDestination = Screen.Welcome.route
     ){
         composable(route = Screen.Splash.route){
             SplashScreen(navController = navController)
         }
         composable(route = Screen.Welcome.route){
-
+            WelcomeScreen(navController = navController)
         }
         composable(route = Screen.Home.route){
 
