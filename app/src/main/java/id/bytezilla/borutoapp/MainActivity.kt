04ce180e254806e8e18c8e620b4baa6208cf4066
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2025-11-14 16:12:03 +0700

    add annotations

--- app/src/main/java/id/bytezilla/borutoapp/MainActivity.kt
+++ app/src/main/java/id/bytezilla/borutoapp/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.pager.ExperimentalPagerApi
 import dagger.hilt.android.AndroidEntryPoint
 import id.bytezilla.borutoapp.navigation.SetupNavGraph
 import id.bytezilla.borutoapp.ui.theme.BorutoAppTheme
 
+@ExperimentalAnimationApi
+@ExperimentalPagerApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
